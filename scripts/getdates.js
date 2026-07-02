6792d977331df8958646b2eed4225ddfa147c8ac
John Chukwu <jstrutacademy@gmail.com>
2026-07-02 22:35:17 +0100

    Complete WDD 131 Week 1 home page assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -8,3 +8,4 @@ document.getElementById("currentyear").textContent = currentYear;
 // Display the date the document was last modified
 document.getElementById("lastModified").textContent =
     `Last Modified: ${document.lastModified}`;
+
