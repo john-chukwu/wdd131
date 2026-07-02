8c76ba538bbebb69c2759f48f4fdf1cb1ae93791
John Chukwu <jstrutacademy@gmail.com>
2026-07-02 22:09:47 +0100

    Complete WDD 131 Week 1 home page assignment

--- README.md
+++
@@ -1,4 +0,0 @@
-# wdd131
-Coursework for WDD 131
-
-This repository contains my coursework for WDD 131.
---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+scripts / getdates.js
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Display the current year in the footer
+document.getElementById("currentyear").textContent = currentYear;
+
+// Display the date the document was last modified
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
